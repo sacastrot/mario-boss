94ac5140f911bda295999b312046acb7813c4967
sacastrot <sctunal@gmail.com>
2023-06-14 14:37:04 -0500

    Bullet enemy basic movement

--- Assets/Scripts/Enemy/FiniteStateMachine/States/PatrolState.cs
+++ Assets/Scripts/Enemy/FiniteStateMachine/States/PatrolState.cs
@@ -6,7 +6,7 @@ public class PatrolState : State
 {
     public override StateType Type { get; }
     public float currentSpeed;
-    public int moveDirection = 1;
+    public int moveDirection = -1;
 
 
     public PatrolState() : base("Patrol")
